291c6d63f36f04074a8054b1399a44b065b5cbc0
BleMᴳᵒᵈツ <86466919+ProBleM15@users.noreply.github.com>
2024-05-05 18:55:26 +0200

    Add files via upload

---
+++ pages-ext/js/recherche.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    // Configuration de l'autocomplétion
+    $('#searchInput').autocomplete({
+        source: function(request, response) {
+            var searchTerm = request.term.toLowerCase();
+            var suggestions = [];
+
+            // Parcourir chaque élément de la liste
+            $('.cs-item').each(function() {
+                var name = $(this).find('.cs-name').text().trim().toLowerCase();
+                var image = $(this).find('.cs-picture img').attr('src');
+                var id = $(this).attr('id');
+
+                // Vérifier si le nom du produit commence par le terme de recherche
+                if (name.startsWith(searchTerm)) {
+                    suggestions.push({
+                        label: '<div class="suggestion-item" data-page="#' + id + '"><img src="' + image + '" alt="' + name + '"><span>' + name + '</span></div>',
+                        value: name,
+                        page: '#' + id
+                    });
+                }
+            });
+
+            response(suggestions);
+        },
+        select: function(event, ui) {
+            var selectedPage = ui.item.page;
+            if (selectedPage) {
+                if (!event.defaultPrevented) {
+                    window.location.href = selectedPage;
+                    $('#searchInput').val('');
+                }
+
+                return false;
+            }
+        }
+    }).autocomplete('instance')._renderItem = function(ul, item) {
+        return $('<li>')
+            .append(item.label)
+            .appendTo(ul);
+    };
+
+    // Gestionnaire d'événements pour le clic sur les suggestions sur les appareils mobiles
+    $(document).on('click touchstart', '.suggestion-item', function(event) {
+        var selectedPage = $(this).data('page');
+        if (selectedPage) {
+            if (!event.defaultPrevented) {
+                window.location.href = selectedPage;
+                $('#searchInput').val('');
+            }
+        }
+    });
+
+    // Empêcher le défilement des suggestions lorsqu'un utilisateur interagit avec la liste de suggestions sur un appareil mobile
+    $('#searchInput').on('touchmove', function(event) {
+        event.preventDefault();
+    });
+});
